72d4b8e581b9e5111a4b2b7d1ad3a608a6005a0c
YooJiho <jhyoo9161@gmail.com>
2020-06-17 16:10:12 +0000

    header 구성

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,55 +1,26 @@
 <template>
-  <div>
-    <nuxt />
-  </div>
+	<sui-menu inverted fixed>	
+		<sui-container>
+			<sui-menu-item is="sui-menu-header">FakeBook</sui-menu-item>
+			<sui-menu-item>
+				<sui-input placeholder="검색어를 입력하세요." icon="search" size="small" />
+			</sui-menu-item>
+
+			<sui-menu-menu position="right">
+				<a is="sui-menu-item">프로필</a>
+				<a is="sui-menu-item">회원가입</a>
+			</sui-menu-menu>
+
+		</sui-container>
+	</sui-menu>
 </template>
 
-<style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
+<script>
+export default {
+  
 }
+</script>
 
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-  margin: 0;
-}
+<style lang="scss" scope>
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
 </style>
